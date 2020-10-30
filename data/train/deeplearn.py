b8796bb8303681d2f220851d0874ec29839557e5
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2020-10-30 12:05:44 +0800

    [add]增加随机森林方法

--- data/train/deeplearn.py
+++ data/train/deeplearn.py
@@ -8,7 +8,9 @@
 import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.ensemble import RandomForestClassifier
+import warnings
+warnings.filterwarnings('ignore')
 # 数据加载
 train_data = pd.read_csv('../../dataset/train.csv')
 test_data = pd.read_csv('../../dataset/test.csv')
@@ -37,6 +39,13 @@ test_data['Fare'].fillna(test_data['Fare'].mean(), inplace=True)
 # 使用登录最多的港口来填充登录港口的 nan 值
 train_data['Embarked'].fillna('S', inplace=True)
 test_data['Embarked'].fillna('S', inplace=True)
+# 将一些字符转换成数字
+combine = [train_data, test_data]
+for dataset in combine:
+    dataset['Embarked'] = dataset['Embarked'].map( {'S': 0, 'C': 1, 'Q': 2} ).astype(int)
+
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)  # 将性别变为数字的格式
 
 """
 构造模型
@@ -67,3 +76,20 @@ pred_labels = clf.predict(test_features)
 # 得到决策树准确率
 acc_decision_tree = round(clf.score(train_features, train_labels), 6)
 print('score 准确率为 %.4lf' % acc_decision_tree)
+
+"""
+使用随机森林进行生死预测
+"""
+x_train = train_data[features]
+y_train = train_data['Survived']
+x_test = test_data[features].copy()
+
+# 选择模型
+clf = RandomForestClassifier(n_estimators=100)
+# 训练
+clf.fit(x_train, y_train)
+# 预测
+predict = clf.predict(x_test)
+clf.score(x_train, y_train)
+# 准确度
+print(round(clf.score(x_train, y_train) * 100, 2))
